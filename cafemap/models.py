8a1754ff0665ee0523c5c8bf6786562d41f6a90e
Elijah Eaton <elijaheaton@elijahs-laptop.mynetworksettings.com>
2024-04-14 15:42:54 -0400

    add cafe page

--- cafemap/models.py
+++ cafemap/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    street_two = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip_code = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.street} {self.street_two}\n{self.city}, {self.state} {self.zip_code}'
+
+class Cafe(models.Model):
+    id = models.CharField(primary_key=True, unique=True, max_length=10)
+    name = models.CharField(max_length=50)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    wifi = models.BooleanField()
+    seating = models.IntegerField(default=0)
+    outdoor_seating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- cafemap/views.py
+++ cafemap/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Cafe
+
+def index(request):
+    cafes = Cafe.objects.order_by("name")
+    return HttpResponse("Cafes:\n{cafes}")
+
+def cafe(request, id):
+    response = f"This is cafe {id}"
+    print(response)
+    cafe = get_object_or_404(Cafe, pk=id)
+    return render(request, 'cafemap/cafe.html', {'cafe': cafe})
